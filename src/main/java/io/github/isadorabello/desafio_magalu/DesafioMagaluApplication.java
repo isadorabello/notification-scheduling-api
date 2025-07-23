c7bae4556245b19983982f656c5a0ebea64ce1c8
isadorabello <isadora.bello@gec.inatel.br>
2025-07-23 18:44:11 -0300

    first commit

---
+++ src/main/java/io/github/isadorabello/desafio_magalu/DesafioMagaluApplication.java
@@ -0,0 +1,13 @@
+package io.github.isadorabello.desafio_magalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMagaluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/isadorabello/desafio_magalu/DesafioMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.isadorabello.desafio_magalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
